fa7ad908478a51c07c03d039ec372189d9fd39be
piksa13 <ania.piktas@gmail.com>
2024-12-12 18:28:03 +0000

    Refactoring, adding another list checker

--- list_of_numbers_check.py
+++ list_of_numbers_check.py
@@ -1,12 +1,24 @@
-def areElementsNumbers(list):
+def are_elements_numbers(initial_list):
     new_list = []
-    return len([new_list.append(i) for i in list if not str(i).isdigit()]) == 0
+    return len([new_list.append(i) for i in initial_list if not str(i).isdigit()]) == 0
+
+# Another solution that stops when non digit is found
+def contains_only_numbers(initial_list):
+    contain_only_digits = True
+    for i in initial_list:
+        if not str(i).isdigit():
+            contain_only_digits = False
+            break
+    return contain_only_digits
 
 list_example_1 = [123, 5, 67, 78, 21, 0, 1, 13, 40, 88, '_*6']
 list_example_2 = [123, 5, 67, 78, 21, 0, 1, 13, 40, 88, '_*']
 list_example_3 = [123, 5, 67, 78, 21, 0, 1, 13, 40, 88, '1ab']
 list_example_4 = [123, 5, 67, 78, 21, 0, 1, 13, 40, 88, '127uy48']
 list_example_5 = [123, 5, 67, 78, 21, 0, 1, 13, 40, 88]
-print(areElementsNumbers(list_example_5))
+list_example_6 = []
+print(are_elements_numbers(list_example_3))
+print(contains_only_numbers(list_example_3))
 
-
+print(are_elements_numbers(list_example_5))
+print(contains_only_numbers(list_example_5))
